a05ef0c425a13d4d628c3e053bb7e49a6d010443
shass005 <safwanhassan013@gmail.com>
2025-11-12 19:48:56 +0000

    Update Create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -1,5 +1,10 @@
 # Create database script for Berties books
 
+# Create the application user 
+CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' 
+IDENTIFIED WITH mysql_native_password BY 'qwertyuiop';
+GRANT ALL PRIVILEGES ON berties_books.* TO 'berties_books_app'@'localhost';
+
 # Create the database
 CREATE DATABASE IF NOT EXISTS berties_books;
 USE berties_books;
